dacbfc7e7aecc3fb53a224ad5a4c6f88ff0c526f
Gurnoor-codecity-YT <80161675+Gurnoor-codecity-YT@users.noreply.github.com>
2021-08-22 01:19:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var canvas, backgroundImage;
+
+var gameState = 0,finishedPlayers;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2;
+var track,gas,cone;
+
+function preload(){
+  track = loadImage("images/track2.png");
+  car1 = loadImage("images/car1.png");
+  gas=loadImage("images/gas.png")
+  cone=loadImage("images/cone.png")
+  
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth , displayHeight);
+  database = firebase.database();
+  gameState = 0;
+  distance = 0;
+ // finishedPlayers = 0;
+  yVel = 0;
+  xVel = 0;
+
+  xSet = false;
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+   //start the game
+   background(200, 200, 255);
+
+   //start the game
+   if (playerCount === 2 ) {
+     game.update(1);
+   }
+ 
+   //start the game for real
+   if (gameState === 1) {
+     game.play();
+   }
+   if (gameState === 2) {
+     console.log("End");
+   }}
+ 
+  
